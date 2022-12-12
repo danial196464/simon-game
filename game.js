8248a7f143770f729342a3cdb5cf47b6209822b5
Danial <107314818+danial196464@users.noreply.github.com>
2022-12-12 22:14:16 +0200

    Files added

---
+++ game.js
@@ -0,0 +1,81 @@
+
+
+var level = 0;
+var firstTimeKey = false;
+$(document).on("keydown",function(){
+  if(!firstTimeKey){
+    firstTimeKey = true;
+    $("h1").text("level "+level);
+    nextSequence();
+  }
+})
+
+var userClickedPattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+  userClickedPattern=[];
+  level++;
+  $("h1").text("level "+level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  playSound(randomChosenColour);
+  animatePress(randomChosenColour);
+
+}
+
+$(".btn").on("click", function () {
+  // var userChosenColour = this.id;
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  console.log(userClickedPattern);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel]==userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(function(){
+        nextSequence();
+      } , 1000);
+    }
+  }else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver(){
+  level = ' ';
+  gamePattern = [];
+  firstTimeKey = false;
+}
